1ae9a1b546d2f877e85975c9768652158066e9d2
safarrr <safar190305@gmail.com>
2024-03-30 16:40:27 +0700

    tambah tuggas tanggal 30-03-24

---
+++ materi/string.py
@@ -0,0 +1,7 @@
+var1 = "Hello world"
+var2 = "I Love Python"
+# print("var1",     )
+default_order = "{},{} dan {}".format("budi","Galih","Ratana")
+print(default_order)
+Positional_order = "{1},{0} dan {2}".format("budi","Galih","Ratana")
+print(Positional_order)
---
+++ tugas/20240330_p3.py
@@ -0,0 +1,40 @@
+# latihan 1
+# Penugasan
+# nilai_pangkat = int(input("masukan Nilai:"))
+# pangkat = int(input("masukan pangkat:"))
+# nilai_pangkat **= pangkat
+# print(nilai_pangkat)
+# Logika
+# is_lulus = True
+# absen_bagus = False
+# if is_lulus and absen_bagus:
+#      print("lulus")
+
+# # Indentitas 
+# x = 10
+# y = 4 
+# indentitas = x & y
+# print("Hasil dari x & y:",indentitas)
+
+# # Keanggotaan
+# var1 = "pyhon"
+# print("y ada di Var1", "y" in var1)
+
+# # penggunaan
+# a = 5
+# b = 5
+# c = 6
+# print('a is b:', a is b)
+
+print("==================TOKO MAINAN==================")
+nama_pembeli = str(input("masukan nama pembeli:"))
+kode_mainan = str(input("masukan kode mainan:"))
+harga = int(input("masukan harga:"))
+jml_beli = int(input("masukan Jumlah Beli:"))
+print("===============================================")
+print("Nama Pembeli     :", nama_pembeli)
+print("Kode mainan      :",kode_mainan)
+print("Harga            :",harga)
+print("Jumlah beli      :",jml_beli)
+print("Total            :", harga * jml_beli)
+print("===============================================")
