9603380d760eef810307e3022f402ba6a439d634
safarrr <safar190305@gmail.com>
2024-04-01 09:12:52 +0700

    tambah tugas logika dan algoritma

---
+++ logika&algoritma/S1P03/tugas1.py
@@ -0,0 +1,24 @@
+# NIM     : 19235176
+# Nama    : Safarudin
+# Kelas   : 19.1A.24
+# Sistem informasi
+
+b1 = 60
+b2 = 20
+b3 = 100
+b4 = 40
+
+max = b1
+bil_terbesar = "b1"
+if b2 > max:
+    max= b2
+    bil_terbesar = "b2"
+
+if b3 > max:
+    max =b3
+    bil_terbesar = "b2"
+if b4 > max:
+    max = b4
+    bil_terbesar = "b4"
+
+print("bilangan terbesar adalah {} dangan jumlah: {}".format(bil_terbesar,max))
