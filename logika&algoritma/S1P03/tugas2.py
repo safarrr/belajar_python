6a72f8168d7d472af739522c4d327ddb5220f807
safarrr <safar190305@gmail.com>
2024-04-01 10:38:09 +0700

    tambah tugas 2 logika & algoritma

---
+++ logika&algoritma/S1P03/tugas2.py
@@ -0,0 +1,13 @@
+# NIM     : 19235176
+# Nama    : Safarudin
+# Kelas   : 19.1A.24
+# Sistem informasi
+aldi = int(input("masuk kelereng aldi:"))
+budi = aldi -15
+anto = 2 * (aldi + budi)
+agung = aldi + budi + anto - 5
+
+print("jumlah kelereng aldi :", aldi)
+print("jumlah kelereng budi :", budi)
+print("jumlah kelereng anton :", anto)
+print("jumlah kelereng agung :", agung)
