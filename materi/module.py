7fcfc53dcf611cb6f0683036bc4070a5279dcc53
safarrr <safar190305@gmail.com>
2024-06-09 10:20:53 +0700

    add natri module

---
+++ materi/module.py
@@ -0,0 +1,5 @@
+import penjumlahan_module as penjumlahan
+
+print(penjumlahan.tambah(10,20))
+print(penjumlahan.pengurangan(20,10))
+print(penjumlahan.perkalian(10,2))
---
+++ materi/penjumlahan_module.py
@@ -0,0 +1,8 @@
+def tambah(a,b):
+    return a + b
+
+def pengurangan(a,b):
+    return a-b
+
+def perkalian(a,b):
+    return a*b
